bc0488ccbf489d379ade7782cb9a01d56216b12e
ArtemSpiridovich <ArtemSpiridovich99@gmail.com>
2020-08-12 13:59:37 +0300

    Updates

---
+++ precache-manifest.2190cb498f3f52ebf569d5427bb35bc6.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852e14b156cc68906d7db4a5c6ae97b0",
+    "url": "/qwerty/index.html"
+  },
+  {
+    "revision": "99e28a73a27588ebbc3f",
+    "url": "/qwerty/static/css/main.db43bffd.chunk.css"
+  },
+  {
+    "revision": "24364144cde28dce8b28",
+    "url": "/qwerty/static/js/2.6fdadb27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qwerty/static/js/2.6fdadb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e28a73a27588ebbc3f",
+    "url": "/qwerty/static/js/main.50c93195.chunk.js"
+  },
+  {
+    "revision": "723ead4bff72e670d722",
+    "url": "/qwerty/static/js/runtime-main.a7debf5d.js"
+  },
+  {
+    "revision": "c9037df475d0e0e372756f1446160d8a",
+    "url": "/qwerty/static/media/Coinpayments.c9037df4.svg"
+  },
+  {
+    "revision": "760a986ad567f3c9be2e80d6be459ac0",
+    "url": "/qwerty/static/media/Vector.760a986a.svg"
+  },
+  {
+    "revision": "9f71843932895465a4772695357dbb5d",
+    "url": "/qwerty/static/media/alipay.9f718439.svg"
+  },
+  {
+    "revision": "919ac270189eb729042ce15da507bd78",
+    "url": "/qwerty/static/media/arrow-left.919ac270.svg"
+  },
+  {
+    "revision": "c475101107bac3d41ba85b0bd4700d27",
+    "url": "/qwerty/static/media/arrow-right.c4751011.svg"
+  },
+  {
+    "revision": "a3c6532e8982e0afa7b90dff764dee2d",
+    "url": "/qwerty/static/media/bch.a3c6532e.svg"
+  },
+  {
+    "revision": "085b513e0750ff2e83debc6cb71af187",
+    "url": "/qwerty/static/media/cart-2.085b513e.svg"
+  },
+  {
+    "revision": "310f122f20f38d044b6ee2832811f85e",
+    "url": "/qwerty/static/media/delete.310f122f.svg"
+  },
+  {
+    "revision": "80175774e9894472ff53f2acdbdcaa9e",
+    "url": "/qwerty/static/media/etp.80175774.svg"
+  },
+  {
+    "revision": "141eef81801cd064ab11d5ecb6c9ef7a",
+    "url": "/qwerty/static/media/favorite.141eef81.svg"
+  },
+  {
+    "revision": "96a4721dc030f2db0ce37a133e67814b",
+    "url": "/qwerty/static/media/feedback.96a4721d.svg"
+  },
+  {
+    "revision": "9294589f705a542d10ad4fa8f357b40d",
+    "url": "/qwerty/static/media/footer-images.9294589f.svg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/qwerty/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "4d64cd8a82ffda189509a0505577e346",
+    "url": "/qwerty/static/media/line.4d64cd8a.svg"
+  },
+  {
+    "revision": "c303052a8169ae1b6308fa2429ee2c27",
+    "url": "/qwerty/static/media/linecart.c303052a.svg"
+  },
+  {
+    "revision": "87ae9244e46bac2b1065725b7598a2ff",
+    "url": "/qwerty/static/media/master-card.87ae9244.svg"
+  },
+  {
+    "revision": "8134c6422dbb1fbb0d970716ab79e89b",
+    "url": "/qwerty/static/media/paypal.8134c642.svg"
+  },
+  {
+    "revision": "366525439141bbd593717218ea790034",
+    "url": "/qwerty/static/media/ppc.36652543.svg"
+  },
+  {
+    "revision": "a12af1c60a6ed7df4e3dd719d22a12fd",
+    "url": "/qwerty/static/media/product-image-2.a12af1c6.svg"
+  },
+  {
+    "revision": "36ee5a3aaa2b2bac3c89af789d989d6f",
+    "url": "/qwerty/static/media/product-image-3.36ee5a3a.svg"
+  },
+  {
+    "revision": "14b537b0fde60a184684552a3ddc4403",
+    "url": "/qwerty/static/media/product-image-4.14b537b0.svg"
+  },
+  {
+    "revision": "38b8d04ac3abdf02337d11e1eb866737",
+    "url": "/qwerty/static/media/product-image-5.38b8d04a.svg"
+  },
+  {
+    "revision": "e610b75e36412b5183953775da3bd698",
+    "url": "/qwerty/static/media/product-image-6.e610b75e.svg"
+  },
+  {
+    "revision": "b4cbb5d0b1524d65e936fc6ba2d04a45",
+    "url": "/qwerty/static/media/product-image.b4cbb5d0.svg"
+  },
+  {
+    "revision": "a12fa0fdb13fbaf2ff4bdfd776cef12a",
+    "url": "/qwerty/static/media/product-line.a12fa0fd.svg"
+  },
+  {
+    "revision": "6713cca32d09515016a0319168c70c1d",
+    "url": "/qwerty/static/media/separator.6713cca3.svg"
+  },
+  {
+    "revision": "7694e9dd83f66695b842d0cec35c0076",
+    "url": "/qwerty/static/media/skrill.7694e9dd.svg"
+  },
+  {
+    "revision": "d0a0002f54b1b8d3a122d7db09e7b9ab",
+    "url": "/qwerty/static/media/square.d0a0002f.svg"
+  },
+  {
+    "revision": "5e462d6f01be4b68b4b444660b83860f",
+    "url": "/qwerty/static/media/tick.5e462d6f.svg"
+  },
+  {
+    "revision": "1332c37a8356db5e92667f13fc7a6458",
+    "url": "/qwerty/static/media/upline.1332c37a.svg"
+  },
+  {
+    "revision": "eb4819150cf4206667d3f79d615ac0ac",
+    "url": "/qwerty/static/media/visa.eb481915.svg"
+  }
+]);
